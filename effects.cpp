6cd3600988a509b1e7878d1e5c603decb16a0041
Tezeusz Woronko <tezeuszw@gmail.com>
2016-12-26 15:27:13 +0100

    parallel revision - omp

--- effects.cpp
+++ effects.cpp
@@ -4,8 +4,7 @@ void defaultOperation(wxImage* result, wxImage* image1, wxImage* image2, int i, 
     return;
 }
 
-void enterLeft(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {
-    unsigned char r, g, b;
+void enterLeft(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {    
     float border = ((float)i)/((float)(n-1));
     border*= image1->GetWidth();
     if(border < 0)
@@ -17,19 +16,20 @@ void enterLeft(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) 
         step = 0;
     if(step > image1->GetWidth())
         step = image1->GetWidth()-1;
-    for(unsigned i = 0; i < image1->GetWidth(); i++) {
-        for(unsigned j = 0; j < image1->GetHeight(); j++) {
-            if(i > border)
-                result->SetRGB(i,j,image1->GetRed(i,j),image1->GetGreen(i,j),image1->GetBlue(i,j));
+    unsigned x, y; 
+    #pragma omp parallel for private(y)
+    for(x = 0; x < image1->GetWidth(); x++) {    
+        for(y = 0; y < image1->GetHeight(); y++) {
+            if(x > border)
+                result->SetRGB(x,y,image1->GetRed(x,y),image1->GetGreen(x,y),image1->GetBlue(x,y));
             else
-                result->SetRGB(i,j,image2->GetRed(i+step,j),image2->GetGreen(i+step,j),image2->GetBlue(i+step,j));
+                result->SetRGB(x,y,image2->GetRed(x+step,y),image2->GetGreen(x+step,y),image2->GetBlue(x+step,y));
         }    
     }
     return;
 }
 
 void enterRight(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {
-    unsigned char r, g, b;
     float border = 1.0 - ((float)i)/((float)(n-1));
     border*= image1->GetWidth();
     if(border < 0)
@@ -41,19 +41,20 @@ void enterRight(wxImage* result, wxImage* image1, wxImage* image2, int i, int n)
         step = 0;
     if(step > image1->GetWidth())
         step = image1->GetWidth()-1;
-    for(unsigned i = 0; i < image1->GetWidth(); i++) {
-        for(unsigned j = 0; j < image1->GetHeight(); j++) {
-            if(i < border)
-                result->SetRGB(i,j,image1->GetRed(i,j),image1->GetGreen(i,j),image1->GetBlue(i,j));
+    unsigned x, y; 
+    #pragma omp parallel for private(y)
+    for(x = 0; x < image1->GetWidth(); x++) {
+        for(y = 0; y < image1->GetHeight(); y++) {
+            if(x < border)
+                result->SetRGB(x,y,image1->GetRed(x,y),image1->GetGreen(x,y),image1->GetBlue(x,y));
             else
-                result->SetRGB(i,j,image2->GetRed(i-step,j),image2->GetGreen(i-step,j),image2->GetBlue(i-step,j));
+                result->SetRGB(x,y,image2->GetRed(x-step,y),image2->GetGreen(x-step,y),image2->GetBlue(x-step,y));
         }    
     }
     return;
 }
 
 void enterUp(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {
-    unsigned char r, g, b;
     float border = ((float)i)/((float)(n-1));
     border*= image1->GetHeight();
     if(border < 0)
@@ -65,19 +66,20 @@ void enterUp(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {
         step = 0;
     if(step > image1->GetHeight())
         step = image1->GetHeight()-1;
-    for(unsigned i = 0; i < image1->GetWidth(); i++) {
-        for(unsigned j = 0; j < image1->GetHeight(); j++) {
-            if(j > border)
-                result->SetRGB(i,j,image1->GetRed(i,j),image1->GetGreen(i,j),image1->GetBlue(i,j));
+    unsigned x, y; 
+    #pragma omp parallel for private(y)
+    for(x = 0; x < image1->GetWidth(); x++) {
+        for(y = 0; y < image1->GetHeight(); y++) {
+            if(y > border)
+                result->SetRGB(x,y,image1->GetRed(x,y),image1->GetGreen(x,y),image1->GetBlue(x,y));
             else
-                result->SetRGB(i,j,image2->GetRed(i,j+step),image2->GetGreen(i,j+step),image2->GetBlue(i,j+step));
+                result->SetRGB(x,y,image2->GetRed(x,y+step),image2->GetGreen(x,y+step),image2->GetBlue(x,y+step));
         }    
     }
     return;
 }
 
 void enterDown(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) {
-    unsigned char r, g, b;
     float border = 1.0 - ((float)i)/((float)(n-1));
     border*= image1->GetHeight();
     if(border < 0)
@@ -89,12 +91,14 @@ void enterDown(wxImage* result, wxImage* image1, wxImage* image2, int i, int n) 
         step = 0;
     if(step > image1->GetHeight())
         step = image1->GetHeight()-1;
-    for(unsigned i = 0; i < image1->GetWidth(); i++) {
-        for(unsigned j = 0; j < image1->GetHeight(); j++) {
-            if(j < border)
-                result->SetRGB(i,j,image1->GetRed(i,j),image1->GetGreen(i,j),image1->GetBlue(i,j));
+    unsigned x, y; 
+    #pragma omp parallel for private(y)
+    for(x = 0; x < image1->GetWidth(); x++) {
+        for(y = 0; y < image1->GetHeight(); y++) {
+            if(y < border)
+                result->SetRGB(x,y,image1->GetRed(x,y),image1->GetGreen(x,y),image1->GetBlue(x,y));
             else
-                result->SetRGB(i,j,image2->GetRed(i,j-step),image2->GetGreen(i,j-step),image2->GetBlue(i,j-step));
+                result->SetRGB(x,y,image2->GetRed(x,y-step),image2->GetGreen(x,y-step),image2->GetBlue(x,y-step));
         }    
     }
     return;
--- effects.h
+++ effects.h
@@ -1,5 +1,8 @@
 #include "projektDlg.h"
 
+#include <omp.h>
+
+
 void defaultOperation(wxImage* result, wxImage* image1, wxImage* image2, int i, int n);
 void enterLeft(wxImage* result, wxImage* image1, wxImage* image2, int i, int n);
 void enterRight(wxImage* result, wxImage* image1, wxImage* image2, int i, int n);
